76b5c873d8d5acf13ee593ae47846d5864cfd1c8
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-17 23:06:37 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
+
 provider "azurerm" {
   # Authentication via service principal using inline variables
   client_id       = "9decc6a2-0185-4bf0-829f-212b9b708ea1"
-  subscription_id = "3e80dd4d-6d0e-48ec-a04b-c1401f2963bb"
+   subscription_id = "3e80dd4d-6d0e-48ec-a04b-c1401f2963bb"
   tenant_id       = "79a50cb4-fa04-495b-9bd1-7860bc00fe6a"
 
   features {}
@@ -9,6 +10,8 @@ provider "azurerm" {
 
 # Define a resource group example
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources3"
+  name     = "example-resources1"
   location = "East US"
 }
+
+
