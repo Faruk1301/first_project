86b7079d1919d589739e8df51f67cbaf0976e86c
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-18 00:20:33 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   features {}
 
   client_id       = "9decc6a2-0185-4bf0-829f-212b9b708ea1"
-  client_secret   = var.client_secret  # This will be passed from GitHub Secrets
+  client_secret   = var.CLIENT_SECRET  # This will be passed from GitHub Secrets
   subscription_id = "3e80dd4d-6d0e-48ec-a04b-c1401f2963bb"
   tenant_id       = "79a50cb4-fa04-495b-9bd1-7860bc00fe6a"
 }
@@ -14,7 +14,6 @@ resource "azurerm_resource_group" "example" {
 }
 
 # Define the variable for client_secret
-variable "client_secret" {}
-
+variable "CLIENT_SECRET" {}
 
 
