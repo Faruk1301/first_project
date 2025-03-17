7012dc2bfdfb52899c67182515955afb0cfe5a22
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-18 01:33:05 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+# Declare the variables
+variable "CLIENT_ID" {
+  description = "The client ID for the Azure AD application."
+  type        = string
+}
+
+variable "CLIENT_SECRET" {
+  description = "The client secret for the Azure AD application."
+  type        = string
+  sensitive   = true
+}
+
+variable "SUBSCRIPTION_ID" {
+  description = "The Azure subscription ID."
+  type        = string
+}
+
+variable "TENANT_ID" {
+  description = "The Azure tenant ID."
+  type        = string
+}
+
+# Provider block
 provider "azurerm" {
   features {}
 
@@ -7,9 +30,9 @@ provider "azurerm" {
   tenant_id       = var.TENANT_ID
 }
 
+# Resource block
 resource "azurerm_resource_group" "example" {
   name     = "example-resources3"
   location = "East US"
 }
-
 
