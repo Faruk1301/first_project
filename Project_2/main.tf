ce4b3a6d35edba2ead843373855c1efb91d46956
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-21 23:30:40 +0600

    Update main.tf

--- Project_2/main.tf
+++ Project_2/main.tf
@@ -1,1 +1,44 @@
+provider "azurerm" {
+  features {}
+
+  client_id       = var.CLIENT_ID
+  client_secret   = var.CLIENT_SECRET
+  subscription_id = var.SUBSCRIPTION_ID
+  tenant_id       = var.TENANT_ID
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.vnet_address_space
+
+  tags = {
+    environment = "Terraform"
+  }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address_prefixes
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "subnet_name" {
+  value = azurerm_subnet.subnet.name
+} 
 
