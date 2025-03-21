8348ed12132af366f9c147128ddd8fbac80626bb
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-21 23:23:24 +0600

    Update varibales.tf

--- Project_2/terraform/varibales.tf
+++ Project_2/terraform/varibales.tf
@@ -1,1 +1,11 @@
+variable "location" {
+  description = "The Azure region to deploy the resources"
+  type        = string
+  default     = "East US"
+}
 
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+  default     = "vnet-terraform"
+}
