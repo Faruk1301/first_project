06e23220e047f1258f275525a5300022cc4c5ac1
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-23 01:10:07 +0600

    Update main.tf

--- Project_2/terraform/main.tf
+++ Project_2/terraform/main.tf
@@ -1,12 +1,39 @@
-provider "azurerm" {
-  features {}
+
+# Declare the necessary variables
+variable "SUBSCRIPTION_ID" {
+  description = "The subscription ID"
 }
 
+variable "CLIENT_ID" {
+  description = "The client ID"
+}
+
+variable "CLIENT_SECRET" {
+  description = "The client secret"
+  sensitive   = true
+}
+
+variable "TENANT_ID" {
+  description = "The tenant ID"
+}
+
+# Use the declared variables in the provider configuration
+provider "azurerm" {
+  features {}
+
+  SUBSCRIPTION_ID = var.SUBSCRIPTION_ID
+  CLIENT_ID       = var.CLIENT_ID
+  CLIENT_SECRET   = var.CLIENT_SECRET
+  TENANT_ID       = var.TENANT_ID
+}
+
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "RG1"
   location = "East US"
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "my-vnet"
   location            = azurerm_resource_group.rg.location
@@ -14,6 +41,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
+# Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "my-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -21,6 +49,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# Output the virtual network name
 output "vnet_name" {
   value = azurerm_virtual_network.vnet.name
 }
