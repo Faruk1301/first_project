76c046e53833fb7f85fba51038097cd757f8696c
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-22 12:19:58 +0600

    Update varibales.tf

--- Project_2/terraform/varibales.tf
+++ Project_2/terraform/varibales.tf
@@ -1,11 +1,51 @@
-variable "location" {
-  description = "The Azure region to deploy the resources"
+# variables.tf
+
+variable "CLIENT_ID" {
+  description = "The Client ID for Azure"
   type        = string
-  default     = "East US"
+}
+
+variable "CLIENT_SECRET" {
+  description = "The Client Secret for Azure"
+  type        = string
+}
+
+variable "SUBSCRIPTION_ID" {
+  description = "The Azure Subscription ID"
+  type        = string
+}
+
+variable "TENANT_ID" {
+  description = "The Azure Tenant ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region"
+  type        = string
 }
 
 variable "vnet_name" {
-  description = "The name of the virtual network"
+  description = "The name of the Virtual Network"
   type        = string
-  default     = "vnet-terraform"
+}
+
+variable "vnet_address_space" {
+  description = "The address space for the Virtual Network"
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "The name of the Subnet"
+  type        = string
+}
+
+variable "subnet_address_prefixes" {
+  description = "The address prefixes for the Subnet"
+  type        = list(string)
 }
